8b940912c11f95277be00d6aadc5430c48475e15
Nekruz Nazirjonov <92411934+nekruz03@users.noreply.github.com>
2022-11-01 03:18:30 -0700

    Delete Web programming/lab1 directory

--- Web programming/lab1/web1/server/restore.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['results'])) {
-    $res = "";
-    for ($i = 0; $i < sizeof($_SESSION['results']); $i++) {
-        $res = $res . $_SESSION['results'][$i] . "#";
-    }
-    echo $res;
-}
